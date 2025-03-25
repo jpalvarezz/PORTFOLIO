766b991d2f424b57c85185c43637359907e211d7
jpalvarezz <joaopedroalvarezsilva@gmail.com>
2025-03-25 12:34:56 -0300

    Atualizando dados de soft e hardskills e languages

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,4 +1,4 @@
-async function fetchProfileData {
+async function fetchProfileData() {
     const url = 'https://raw.githubusercontent.com/jpalvarezz/PORTFOLIO/refs/heads/main/data/profile.json';
     const fetching = await fetch(url);
     return await fetching.json();
